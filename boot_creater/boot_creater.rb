9e122d90cd021fdbc4db7fbf0c02ab92f11a2c7a
Abel Gancsos <agancsos@users.noreply.github.com>
2022-01-24 22:53:45 -0500

    Create boot_creater.rb

---
+++ boot_creater/boot_creater.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+###############################################################################
+# Name        : boot_creater.rb                                               #
+# Author      : Abel Gancsos                                                  #
+# Version     : v. 1.0.0.0                                                    #
+# Description : Helps to create a bootable USB install driver..               #
+###############################################################################
+
+class BootCreater
+    attr_accessor :image_path; attr_accessor :dev_path;@debug=nil;
+    def initialize(params={})
+        @image_path = params["-i"] != nil ? params["-i"] : "";
+        @dev_path   = params["-p"] != nil ? params["-p"] : "";
+        @debug      = params["--debug"] != nil and params["--debug"].to_i > 0 ? true : false;
+    end
+    def invoke()
+        raise "Dev path cannot be empty..." if @dev_path == "";
+        raise "Image path cannot be empty and must exist..." if @image_path == "" or !File.exist?(@image_path);
+        raise "Script must be ran as sudo..." if `whoami`.strip != "root";
+        image_name = File.basename(@image_path).split(".")[0];
+        if (!@debug)
+            puts(`diskutil eraseDisk HFS+ <drive-target-name> <dev-path>`);
+            puts(`sudo "#{@image_path}/Contents/Resources/createinstallmedia" --volume "/Volume/#{image_path}"`);
+        end
+    end
+end
+
+params = {};
+for i in 0..ARGV.length do params[ARGV[i]] = ARGV[i + 1] end
+session = BootCreater.new(params);
+session.invoke();

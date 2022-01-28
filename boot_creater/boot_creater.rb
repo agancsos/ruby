c4f9d2fbe645936776a6ab769924ef9f4d332622
Abel Gancsos <agancsos@users.noreply.github.com>
2022-01-28 10:00:21 -0500

    Update boot_creater.rb

--- boot_creater/boot_creater.rb
+++ boot_creater/boot_creater.rb
@@ -7,20 +7,35 @@
 ###############################################################################
 
 class BootCreater
-    attr_accessor :image_path; attr_accessor :dev_path;@debug=nil;
+    @image_path=nil;@dev_path=nil;@debug=nil;@iso=nil;
     def initialize(params={})
         @image_path = params["-i"] != nil ? params["-i"] : "";
         @dev_path   = params["-p"] != nil ? params["-p"] : "";
         @debug      = params["--debug"] != nil and params["--debug"].to_i > 0 ? true : false;
+        @iso        = params["--iso"] != nil and params["--iso"].to_i > 0 ? true : false;
     end
     def invoke()
         raise "Dev path cannot be empty..." if @dev_path == "";
         raise "Image path cannot be empty and must exist..." if @image_path == "" or !File.exist?(@image_path);
         raise "Script must be ran as sudo..." if `whoami`.strip != "root";
         image_name = File.basename(@image_path).split(".")[0];
-        if (!@debug)
-            puts(`diskutil eraseDisk HFS+ "#{image_name}" #{@dev_path}`);
-            puts(`sudo "#{@image_path}/Contents/Resources/createinstallmedia" --volume "/Volume/#{image_name}"`);
+        image_size = (File.size("#{@image_path}/Contents/SharedSupport/SharedSupport.dmg") * 0.000001 / 1024 + 1).to_i;
+        if (not @iso)
+                puts("Building bootable USB");
+                if (not @debug)
+                    puts(`diskutil eraseDisk HFS+ "#{image_name}" #{@dev_path}`);
+                    puts(`sudo "#{@image_path}/Contents/Resources/createinstallmedia" --volume "/Volume/#{image_name}"`);
+                end
+        else
+            puts("Building bootable ISO");
+            if (not @debug)
+                puts(`hdiutil create -o "/tmp/#{image_name}" -size #{image_size} -volname #{image_name} -layout SPUD -fs HFS+J`);
+                puts(`hdiutil attach "/tmp/#{image_name}.dmg" -noverify -mountpoint "/Volumes/#{image_name}"`);
+                puts(`sudo "#{@image_path}/Contents/Resources/createinstallmedia" --volume "/Volume/#{image_name}" --nointeraction`);
+                puts(`hdutil detach "/Volumes/#{image_name}"`);
+                puts(`hdiutil convert "/tmp/#{image_name}.dmg" -format UDTO -o "/tmp/#{image_name}.iso"`);
+                puts(`mv "/tmp/#{image_name}.iso.cdr" "/tmp/#{image_name}.iso"`);
+            end
         end
     end
 end
@@ -29,3 +44,4 @@ params = {};
 for i in 0..ARGV.length do params[ARGV[i]] = ARGV[i + 1] end
 session = BootCreater.new(params);
 session.invoke();
+
